db25d2afb1d76270b7d7b051e740b13e497041b6
tangtao <tangtao@newrank.cn>
2020-08-21 21:44:19 +0800

    feat: 🎸 mock

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-function sum(a, b) {
-  return a + b;
+function forEach(items, callback) {
+  for (let index = 0; index < items.length; index++) {
+    callback(items[index]);
+  }
 }
 
-module.exports = sum;
+module.exports = forEach;
--- index.test.js
+++ index.test.js
@@ -1,84 +1,25 @@
-const sum = require("./index");
+const forEach = require("./index");
 
-// Matchers
-test("adds 1 + 2 to equal 3", () => {
-  expect(sum(1, 2)).toBe(3);
+const mockCallback = jest.fn((x) => 42 + x);
+forEach([0, 1], mockCallback);
+
+test("此 mock 函数被调用了两次", () => {
+  // console.log(mockCallback.mock);
+  expect(mockCallback.mock.calls.length).toBe(2);
 });
 
-test("object assignment", () => {
-  const data = { one: 1 };
-  data["two"] = 2;
-  expect(data).toEqual({ one: 1, two: 2 });
+test("第一次调用函数时的第一个参数是 0", () => {
+  expect(mockCallback.mock.calls[0][0]).toBe(0);
 });
 
-// Truthiness
-test("null", () => {
-  const n = null;
-  expect(n).toBeNull();
-  expect(n).toBeDefined();
-  expect(n).not.toBeUndefined();
-  expect(n).not.toBeTruthy();
-  expect(n).toBeFalsy();
+test("第二次调用函数时的第一个参数是 1", () => {
+  expect(mockCallback.mock.calls[1][0]).toBe(1);
 });
 
-test("zero", () => {
-  const z = 0;
-  expect(z).not.toBeNull();
-  expect(z).toBeDefined();
-  expect(z).not.toBeUndefined();
-  expect(z).not.toBeTruthy();
-  expect(z).toBeFalsy();
+test("第一次函数调用的返回值是 42", () => {
+  expect(mockCallback.mock.results[0].value).toBe(42);
 });
 
-// Numbers
-test("two plus two", () => {
-  const value = 2 + 2;
-  expect(value).toBeGreaterThan(3);
-  expect(value).toBeGreaterThanOrEqual(3.5);
-  expect(value).toBeLessThan(5);
-  expect(value).toBeLessThanOrEqual(4.5);
-
-  // toBe and toEqual are equivalent for numbers
-  expect(value).toBe(4);
-  expect(value).toEqual(4);
-});
-test("adding floating point numbers", () => {
-  const value = 0.1 + 0.2;
-  //expect(value).toBe(0.3);           This won't work because of rounding error
-  expect(value).toBeCloseTo(0.3); // This works.
-});
-
-// Strings
-test("there is no I in team", () => {
-  expect("team").not.toMatch(/I/);
-});
-
-test('but there is a "stop" in Christoph', () => {
-  expect("Christoph").toMatch(/stop/);
-});
-
-//Arrays
-const shoppingList = [
-  "diapers",
-  "kleenex",
-  "trash bags",
-  "paper towels",
-  "beer",
-];
-test("the shopping list has beer on it", () => {
-  expect(shoppingList).toContain("beer");
-  expect(new Set(shoppingList)).toContain("beer");
-});
-
-//error
-function compileAndroidCode() {
-  throw new Error("you are using the wrong JDK");
-}
-
-test("compiling android goes as expected", () => {
-  expect(compileAndroidCode).toThrow();
-  expect(compileAndroidCode).toThrow(Error);
-  // You can also use the exact error message or a regexp
-  expect(compileAndroidCode).toThrow("you are using the wrong JDK");
-  expect(compileAndroidCode).toThrow(/JDK/);
+test("第二次函数调用的返回值是 43", () => {
+  expect(mockCallback.mock.results[1].value).toBe(43);
 });
