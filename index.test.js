079d4ef7aeec0ca991d2d06db2fee49ab1a008be
tangtao <tangtao@newrank.cn>
2020-08-21 20:50:37 +0800

    生命周期

--- index.test.js
+++ index.test.js
@@ -1,84 +1,56 @@
-const sum = require("./index");
-
-// Matchers
-test("adds 1 + 2 to equal 3", () => {
-  expect(sum(1, 2)).toBe(3);
+beforeEach(() => {
+  console.log("beforeEach");
 });
 
-test("object assignment", () => {
-  const data = { one: 1 };
-  data["two"] = 2;
-  expect(data).toEqual({ one: 1, two: 2 });
+afterEach(() => {
+  console.log("afterEach");
 });
 
-// Truthiness
-test("null", () => {
-  const n = null;
-  expect(n).toBeNull();
-  expect(n).toBeDefined();
-  expect(n).not.toBeUndefined();
-  expect(n).not.toBeTruthy();
-  expect(n).toBeFalsy();
+test("toBeTruthy", () => {
+  console.log("toBeTruthy");
+  expect("toBeTruthy").toBeTruthy();
 });
 
-test("zero", () => {
-  const z = 0;
-  expect(z).not.toBeNull();
-  expect(z).toBeDefined();
-  expect(z).not.toBeUndefined();
-  expect(z).not.toBeTruthy();
-  expect(z).toBeFalsy();
+// 作用域
+describe("describe", () => {
+  // Applies only to tests in this describe block
+  beforeEach(() => {
+    console.log("describe:beforeEach");
+  });
+
+  test("3>2", () => {
+    console.log("describe:toBeTruthy");
+    expect(3 > 2).toBe(true);
+  });
+
+  afterEach(() => {
+    console.log("describe:afterEach");
+  });
 });
 
-// Numbers
-test("two plus two", () => {
-  const value = 2 + 2;
-  expect(value).toBeGreaterThan(3);
-  expect(value).toBeGreaterThanOrEqual(3.5);
-  expect(value).toBeLessThan(5);
-  expect(value).toBeLessThanOrEqual(4.5);
+// beforeAll(() => console.log("1 - beforeAll"));
+// afterAll(() => console.log("1 - afterAll"));
+// beforeEach(() => console.log("1 - beforeEach"));
+// afterEach(() => console.log("1 - afterEach"));
+// test("", () => console.log("1 - test"));
 
-  // toBe and toEqual are equivalent for numbers
-  expect(value).toBe(4);
-  expect(value).toEqual(4);
-});
-test("adding floating point numbers", () => {
-  const value = 0.1 + 0.2;
-  //expect(value).toBe(0.3);           This won't work because of rounding error
-  expect(value).toBeCloseTo(0.3); // This works.
-});
+// describe("Scoped / Nested block", () => {
+//   beforeAll(() => console.log("2 - beforeAll"));
+//   afterAll(() => console.log("2 - afterAll"));
+//   beforeEach(() => console.log("2 - beforeEach"));
+//   afterEach(() => console.log("2 - afterEach"));
+//   test("", () => console.log("2 - test"));
+// });
 
-// Strings
-test("there is no I in team", () => {
-  expect("team").not.toMatch(/I/);
-});
-
-test('but there is a "stop" in Christoph', () => {
-  expect("Christoph").toMatch(/stop/);
-});
-
-//Arrays
-const shoppingList = [
-  "diapers",
-  "kleenex",
-  "trash bags",
-  "paper towels",
-  "beer",
-];
-test("the shopping list has beer on it", () => {
-  expect(shoppingList).toContain("beer");
-  expect(new Set(shoppingList)).toContain("beer");
-});
-
-//error
-function compileAndroidCode() {
-  throw new Error("you are using the wrong JDK");
-}
-
-test("compiling android goes as expected", () => {
-  expect(compileAndroidCode).toThrow();
-  expect(compileAndroidCode).toThrow(Error);
-  // You can also use the exact error message or a regexp
-  expect(compileAndroidCode).toThrow("you are using the wrong JDK");
-  expect(compileAndroidCode).toThrow(/JDK/);
-});
+// 1 - beforeAll
+// 1 - beforeEach
+// 1 - test
+// 1 - afterEach
+// 2 - beforeAll
+// 1 - beforeEach
+// 2 - beforeEach
+// 2 - test
+// 2 - afterEach
+// 1 - afterEach
+// 2 - afterAll
+// 1 - afterAll
